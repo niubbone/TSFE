c2ef9b8f09f3397d84f5f3fb16b07e4c85ab815c
niubbone <guren@tiscali.it>
2026-01-17 15:41:12 +0100

    Update version.js

--- version.js
+++ version.js
@@ -17,7 +17,7 @@
 // ============================================
 // CAMBIA SOLO QUESTO NUMERO
 // ============================================
-export const VERSION = '4.2.1';
+export const VERSION = '4.2.1b';
 // ============================================
 
 // DOPO aver cambiato VERSION sopra:
