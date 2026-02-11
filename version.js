05613b84d86f46f02321087a6be1e5e69c8ad8e4
niubbone <guren@tiscali.it>
2026-02-11 21:18:29 +0100

    Update version number to 4.2.5

--- version.js
+++ version.js
@@ -17,7 +17,7 @@
 // ============================================
 // CAMBIA SOLO QUESTO NUMERO
 // ============================================
-export const VERSION = '4.2.4';
+export const VERSION = '4.2.5';
 // ============================================
 
 // DOPO aver cambiato VERSION sopra:
